3ed3d89c8d1ff063f7b81f1d7791f87386f8ab49
Tan Tran <t16021999@gmail.com>
2023-11-29 01:13:10 +0700

    useEffect for setTimeout and setInterval
    q
    
    /

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,20 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
-  const dialog = useRef();
+const Modal = ({ open, children }) => {
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  const dialog = useRef();
+  useEffect(() => {
+    open ? dialog.current.showModal() : dialog.current.close()
+  }, [open])
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
+    <dialog className="modal" ref={dialog} >
+
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
-  );
-});
+  )
+}
 
 export default Modal;
